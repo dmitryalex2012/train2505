62a4fde12b96eb234180dd9040984f10c6f9f92e
DmitryAlex2012 <tpmfd27@gmail.com>
2020-05-25 15:25:15 +0300

    Add "Label" to index page.

--- views/site/index.php
+++ views/site/index.php
@@ -20,8 +20,8 @@ $this->title = 'My Yii Application';
         <?=     Html::a('New curtains', ['curtains/index'], ['class' => 'btn btn-lg btn-success']);
         ?>
 
-        <label>Master</label>
-        <label>Branch</label>
+        <br>
+        <label>Branch: "make New Buttons"</label>
 
     </div>
 
