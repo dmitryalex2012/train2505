2bfd6f3dfdc52f6c9034ccaa805ee80ccd86810b
DmitryAlex2012 <tpmfd27@gmail.com>
2020-05-25 14:47:02 +0300

    Change "Congratulations" button on "Curtains" project.

--- views/site/index.php
+++ views/site/index.php
@@ -2,6 +2,9 @@
 
 /* @var $this yii\web\View */
 
+use yii\helpers\Html;
+
+
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
@@ -11,7 +14,9 @@ $this->title = 'My Yii Application';
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <?=   Html::a('Curtains', ['curtains/index'], ['class' => 'btn btn-lg btn-success']);
+        ?>
+
     </div>
 
     <div class="body-content">
